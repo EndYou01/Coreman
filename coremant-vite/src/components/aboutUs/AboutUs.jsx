cf8dcbeca364de135da59b51b4e9fb4b1778a930
Eduardo Javier Garcia Alfonso <eduardojga.oficina@gmail.com>
2023-03-04 14:27:53 -0500

    about section finished

--- coremant-vite/src/components/aboutUs/AboutUs.jsx
+++ coremant-vite/src/components/aboutUs/AboutUs.jsx
@@ -2,6 +2,16 @@ import React from 'react'
 
 export const AboutUs = () => {
   return (
-    <div>AboutUs</div>
+    <div className='about-container'>
+      
+      <h1>
+        ABOUT US
+      </h1>
+
+      <div>
+        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Error facilis quia nulla aliquid deserunt laboriosam aspernatur suscipit eos molestiae reiciendis! Quas deserunt quis animi. Est pariatur unde repellendus temporibus deserunt!</p>
+      </div>
+
+    </div>
   )
 }
--- coremant-vite/src/pages/IndexPage.jsx
+++ coremant-vite/src/pages/IndexPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { AboutUs } from '../components/aboutUs/AboutUs'
 import { Banner } from '../components/banner/Banner'
 
 export const IndexPage = () => {
   return (
     <div>
       <Banner />
+      <AboutUs />
     </div>
   )
 }
