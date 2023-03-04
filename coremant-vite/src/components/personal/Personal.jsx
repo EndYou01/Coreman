3f7759c6426809dbf7005a76981d86d74d8ad2f8
Eduardo Javier Garcia Alfonso <eduardojga.oficina@gmail.com>
2023-03-04 16:20:50 -0500

    personal section finished

--- coremant-vite/src/components/personal/Personal.jsx
+++ coremant-vite/src/components/personal/Personal.jsx
@@ -1,7 +1,76 @@
-import React from 'react'
+import React from "react";
+import { TiSocialTwitter } from "react-icons/ti";
+import { FaWhatsapp } from "react-icons/fa";
+import { TfiFacebook } from "react-icons/tfi";
 
 export const Personal = () => {
-  return (
-    <div>Personal</div>
-  )
-}
+	return (
+		<div className="personal-container">
+			<h1>Personal</h1>
+
+			<div className="personal_info">
+				<div className="img_personal-container">{/* IMAGEN */}</div>
+
+				<h2>Alberto A. Dupotey Angulo</h2>
+				<h3>Director General</h3>
+
+				<div>
+					<ul className="list">
+						<li>
+							<a href="#">
+								<TiSocialTwitter
+									style={{ color: "#00acee", fontSize: "5rem" }}
+								/>
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<FaWhatsapp style={{ color: "#25D366", fontSize: "4.2rem" }} />
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<TfiFacebook
+									style={{ color: " #3b5998", fontSize: "3.2rem" }}
+								/>
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+
+			<div className="personal_info">
+				<div className="img_personal-container">{/* IMAGEN */}</div>
+
+				{/* subdirector */}
+
+				<h2>Leandro Camejo Grau</h2>
+				<h3>Sub Director</h3>
+
+				<div>
+					<ul className="list">
+						<li>
+							<a href="#">
+								<TiSocialTwitter
+									style={{ color: "#00acee", fontSize: "5rem" }}
+								/>
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<FaWhatsapp style={{ color: "#25D366", fontSize: "4.2rem" }} />
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<TfiFacebook
+									style={{ color: " #3b5998", fontSize: "3.2rem" }}
+								/>
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
+};
--- coremant-vite/src/pages/IndexPage.jsx
+++ coremant-vite/src/pages/IndexPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { AboutUs } from '../components/aboutUs/AboutUs'
 import { Banner } from '../components/banner/Banner'
+import { Personal } from '../components/personal/Personal'
 
 export const IndexPage = () => {
   return (
     <div>
       <Banner />
       <AboutUs />
+      <Personal />
     </div>
   )
 }
