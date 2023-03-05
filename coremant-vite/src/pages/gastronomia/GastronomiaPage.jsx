caf52414516ccc1084df5bdb3989fcea6d06261f
Eduardo Javier Garcia Alfonso <eduardojga.oficina@gmail.com>
2023-03-05 01:58:11 -0500

    all peges finished

--- coremant-vite/src/pages/gastronomia/GastronomiaPage.jsx
+++ coremant-vite/src/pages/gastronomia/GastronomiaPage.jsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import React from "react";
+import foodLogo from "../../assets/foodx2.png";
 
 export const GastronomiaPage = () => {
-  return (
-    <div>GastronomiaPage</div>
-  )
-}
+	return (
+		<div>
+			<header className="header">
+				<div className="gastronomia_bg"></div>
+				<div className="header_opacity"></div>
+				<div className="header_info">
+					<div >
+						<button>{"<"} Atras</button>
+					</div>
+
+					<div className="logo_page-container">
+						<img src={foodLogo} alt="gastronomia logo" />
+					</div>
+				</div>
+			</header>
+
+			<div className="page_info-container">
+				<h1>Materiales de Oficina</h1>
+
+				<div>
+					<p>
+						{" "}
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+						facilis quia nulla aliquid deserunt laboriosam aspernatur suscipit
+						eos molestiae reiciendis! Quas deserunt quis animi. Est pariatur
+						unde repellendus temporibus deserunt!
+					</p>
+				</div>
+			</div>
+			<div className="ver_catalogos-container">
+				<h1>VER CATALOGOS</h1>
+				<p>
+					We're a small creative design studio based in New York, working with
+					adventurous clients and providing Creative Concepts, Branding, Graphic
+					Design, Art Direction and Digital Communication services.
+				</p>
+				<button>SEE</button>
+			</div>
+		</div>
+	);
+};
