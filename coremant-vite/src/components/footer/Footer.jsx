180684c0ed2548667c970c85941e8b2d7204e6ab
EndYou <jorgesenjudo@gmail.com>
2023-03-05 04:34:02 -0500

    Realizando mejoras en cuanto a diseño y en el cdgo

--- coremant-vite/src/components/footer/Footer.jsx
+++ coremant-vite/src/components/footer/Footer.jsx
@@ -14,41 +14,53 @@ export const Footer = () => {
 
 	return (
 		<footer className="footer-container">
-			<div className="img_footer-container">
-				<img 
-					src={coremantLogo} 
-					alt="loco icon" 
-					onClick={() => {
-						navigate('/') 
-						scrollToTop()
-					}} />
+			
+			<div className="footer_contain">
+				
+				<div className="img_footer-container">
+					<img
+						src={coremantLogo}
+						alt="loco icon"
+						onClick={() => {
+							navigate('/')
+							scrollToTop()
+						}} />
+				</div>
+
+				<div>
+					<h1>CONTACTANOS</h1>
+					<p>
+						sample text. Click to select the text box.click again or blab lablab
+						jajaja eran textos de ejemplo
+					</p>
+				</div>
+
+				<div>
+					<h1>SIGUENOS</h1>
+					<ul className='list'>
+						<li>
+							<a href="#">
+								<BsYoutube style={{ color: "#c4302b", fontSize: "3.7rem" }} />
+							</a>
+						</li>
+
+						<li>
+							<a href="#">
+								<FaFacebook style={{ color: "#3b5998", fontSize: "3.2rem" }} />
+							</a>
+						</li>
+
+
+						<li>
+							<a href="#">
+								<AiFillInstagram style={{ color: "#C13584", fontSize: "3.7rem" }} />
+							</a>
+						</li>
+					</ul>
+				</div>
+				
 			</div>
-			<h1>CONTACTANOS</h1>
-			<p>
-				sample text. Click to select the text box.click again or blab lablab
-				jajaja eran textos de ejemplo
-			</p>
-			<h1>SIGUENOS</h1>
-			<ul className='list'>
-				<li>
-					<a href="#">
-						<BsYoutube style={{ color: "#c4302b", fontSize: "3.7rem" }} />
-					</a>
-				</li>
 
-				<li>
-					<a href="#">
-						<FaFacebook style={{ color: "#3b5998", fontSize: "3.2rem" }} />
-					</a>
-				</li>
-
-
-				<li>
-					<a href="#">
-						<AiFillInstagram style={{ color: "#C13584", fontSize: "3.7rem" }} />
-					</a>
-				</li>
-			</ul>
 			<p>@{fecha} Coremant todos los derechos reservados</p>
 		</footer>
 	);
--- coremant-vite/src/components/personal/Personal.jsx
+++ coremant-vite/src/components/personal/Personal.jsx
@@ -5,72 +5,76 @@ import { TfiFacebook } from "react-icons/tfi";
 
 export const Personal = () => {
 	return (
+		<>
+		<h1 className="personal_h1">PERSONAL</h1>
 		<div className="personal-container">
-			<h1>PERSONAL</h1>
 
-			<div className="personal_info">
-				<div className="img_personal-container">{/* IMAGEN */}</div>
+			<div className="personal_info_container">
+				<div className="personal_info">
+					<div className="img_personal-container">{/* IMAGEN */}</div>
 
-				<h2>Alberto A. Dupotey Angulo</h2>
-				<h3>Director General</h3>
+					<h2>Alberto A. Dupotey Angulo</h2>
+					<h3>Director General</h3>
 
-				<div>
-					<ul className="list">
-						<li>
-							<a href="#">
-								<TiSocialTwitter
-									style={{ color: "#00acee", fontSize: "5rem" }}
-								/>
-							</a>
-						</li>
-						<li>
-							<a href="#">
-								<FaWhatsapp style={{ color: "#25D366", fontSize: "4.2rem" }} />
-							</a>
-						</li>
-						<li>
-							<a href="#">
-								<TfiFacebook
-									style={{ color: " #3b5998", fontSize: "3.2rem" }}
-								/>
-							</a>
-						</li>
-					</ul>
+					<div>
+						<ul className="list">
+							<li>
+								<a href="#">
+									<TiSocialTwitter
+										style={{ color: "#00acee", fontSize: "5rem" }}
+									/>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<FaWhatsapp style={{ color: "#25D366", fontSize: "4.2rem" }} />
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<TfiFacebook
+										style={{ color: " #3b5998", fontSize: "3.2rem" }}
+									/>
+								</a>
+							</li>
+						</ul>
+					</div>
 				</div>
-			</div>
 
-			<div className="personal_info">
-				<div className="img_personal-container">{/* IMAGEN */}</div>
+				<div className="personal_info">
+					<div className="img_personal-container">{/* IMAGEN */}</div>
 
-				{/* subdirector */}
+					{/* subdirector */}
 
-				<h2>Leandro Camejo Grau</h2>
-				<h3>Sub Director</h3>
+					<h2>Leandro Camejo Grau</h2>
+					<h3>Sub Director</h3>
 
-				<div>
-					<ul className="list">
-						<li>
-							<a href="#">
-								<TiSocialTwitter
-									style={{ color: "#00acee", fontSize: "5rem" }}
-								/>
-							</a>
-						</li>
-						<li>
-							<a href="#">
-								<FaWhatsapp style={{ color: "#25D366", fontSize: "4.2rem" }} />
-							</a>
-						</li>
-						<li>
-							<a href="#">
-								<TfiFacebook
-									style={{ color: " #3b5998", fontSize: "3.2rem" }}
-								/>
-							</a>
-						</li>
-					</ul>
+					<div>
+						<ul className="list">
+							<li>
+								<a href="#">
+									<TiSocialTwitter
+										style={{ color: "#00acee", fontSize: "5rem" }}
+									/>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<FaWhatsapp style={{ color: "#25D366", fontSize: "4.2rem" }} />
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<TfiFacebook
+										style={{ color: " #3b5998", fontSize: "3.2rem" }}
+									/>
+								</a>
+							</li>
+						</ul>
+					</div>
 				</div>
 			</div>
 		</div>
+		</>
 	);
 };
