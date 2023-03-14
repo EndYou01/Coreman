62124a7ac4c3e31a7aaaab94effa1f8088db3a26
EndYou <jorgesenjudo@gmail.com>
2023-03-14 10:59:30 -0500

    Actualizando informacion del About Us

--- coremant-vite/src/components/aboutUs/AboutUs.jsx
+++ coremant-vite/src/components/aboutUs/AboutUs.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 
 export const AboutUs = () => {
-  return (
-    <div className='about-container'>
-      
-      <h1>
-        ABOUT US
-      </h1>
+	return (
+		<div className='about-container'>
 
-      <div>
-        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Error facilis quia nulla aliquid deserunt laboriosam aspernatur suscipit eos molestiae reiciendis! Quas deserunt quis animi. Est pariatur unde repellendus temporibus deserunt!</p>
-      </div>
+			<h1>
+				ABOUT US
+			</h1>
 
-    </div>
-  )
+			<div>
+				<p>
+					Empresa privada cuyo objeto principal es brindar servicios de actividades especializadas de la construcción, reparación y mantenimiento a inmuebles, a la vez comercializa materiales, herramientas y útiles para la construcción, partes y piezas de carpintería para edificios y construcciones, pinturas, barnices y productos de revestimientos similares, tintas de imprenta y masillas y servicios de actividades de decoradores de interiores. </p>
+				<p>
+					Desarrollamos servicios de gastronomía a través de una red de Cafeterías-Bares-Restaurantes y servicios a domicilios que tiene como objetivo ofertar a los clientes una amplia variedad de alimentos y bebidas, entre ellos se destacan los entrepanes, los platos hechos a base de diferentes tipos de carnes acompañados de viandas en sus variedades y ensaladas de vegetales, refrescos nacionales e importados, helados, confituras, jugos naturales, cervezas y maltas, entre otros.
+				</p>
+			</div>
+
+		</div>
+	)
 }
